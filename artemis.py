3699c9e4fef3a9425806dc90f5c6586d45f1cb8b
r3cursive123 <r3cursive@null.net>
2018-10-03 22:24:58 -0400

    Add files via upload

--- artemis.py
+++ artemis.py
@@ -16,6 +16,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+proto = ""
 
 # Loop through ip file and visit pages
 with open('ips.txt','r') as f1:
@@ -24,19 +25,24 @@ with open('ips.txt','r') as f1:
         port = line.strip().split(':'[0])
         port = port[1]
         if port == '443':
-            line = 'https://' + line
-            print line
+            proto = 'https://'
+            line =  proto + line
         elif port == '80':
-            line = 'http://' + line
-            print line
+            proto = 'http://'
+            line = proto + line
         elif port == '8080':
-            line = 'http://' + line
-            print line
-        else:
-            print ("""The following page could not be visited: %s\n(The port may not be added to this script yet)""") % (line)
+            proto = 'http://'
+            line = proto + line
+        elif port == '8443':
+            proto = 'https://'
+            line = proto + line
         try:
             driver.get(line)
             out_file = (line.strip().replace(':','%3A').replace('/','%2F') + '.png')
             driver.get_screenshot_as_file(str(out_file))
-        except:
-            pass
+            print "Screenshot of " + proto + line + " created successfully!"
+        except Exception as e:
+            print "We got a problem..."
+            print (
+                  """The following page could not be visited: %s\n(The port may not be added to this script yet)""") % (line)
+            print e
